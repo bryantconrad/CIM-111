16703c230782a8211c4dbc7ec88482203df4c7c4
bryantconrad <bryant.conradl@gmail.com>
2018-11-12 22:04:47 -0500

    Finished a5 but was lazy with some things

--- a5/assets/js/script.js
+++ a5/assets/js/script.js
@@ -2,26 +2,50 @@ $(document).ready(function() {
 
     $(".desc").hide();
 
+    //if/else statement and functions (show/hide events)
+
     var hideStatus = true;
 
     $("#showbtn").click(function(){
         if(hideStatus == true){
-
             $(".desc").slideDown(300);
             hideStatus = false;
             $("#showbtn").html("Hide all");
-
         }
         else{
-
             $(".desc").slideUp(300);
             hideStatus = true;
             $("#showbtn").html("Show all");
-
         }
     });
 
-    //list sliding
+    //for loop, basically used as a complicated way to show/hide here because I couldn't think of a better way to use a for loop
+
+    var i;
+    var guitars = ["Gibson", "Fender", "Ibanez", "Les Paul", "Yamaha"];
+    var text = "";
+    brandArray = false;
+
+    $("#brands").click(function(){
+        if(brandArray == false){
+            for (i = 0; i < guitars.length; i++) {
+                text += guitars[i] + "<br>";
+            };
+            $("#guitars").html(text);
+            $("#brands").html("Hide brands");
+            brandArray = true;
+            
+        }
+        else{
+            text = "";
+            $("#guitars").html(text);
+            $("#brands").html("Show brands");
+            brandArray = false;
+        }
+    });
+
+
+    //list sliding (show/hide events)
 
     $(".item1").on({
         click: function(){
@@ -82,31 +106,5 @@ $(document).ready(function() {
             $(".item5").css("background-color", "white");
         }
     });
-
-
-
-    /*
-    $(".item1").click(function() {
-        if(hideStatus == true){
-            $(".body1").hide(300);
-            hideStatus = false;
-        }
-        else{
-            $(".body1").show(300);
-            hideStatus = true;
-        }
-    });
-
-    $(".item2").click(function() {
-        if(hideStatus == true){
-            $(".body2").hide(300);
-            hideStatus = false;
-        }
-        else{
-            $(".body2").show(300);
-            hideStatus = true;
-        }
-    });
-    */
 
 });
